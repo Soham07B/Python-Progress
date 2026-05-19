2d396f3eaece754ac01fde8e5ea55ba7483fad58
Soham Biswas <sohambis.in@gmail.com>
2026-05-19 23:45:34 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -62,3 +62,44 @@ x=y=z= "orange"
 fruits=["apple","orange","banana"]
 x,y,z=fruits
 
+x="Awesome"
+def myFnc():
+    print("Python is ", x);
+myFnc()
+
+def func():
+    global x    #Assigning a variale as ghlobal within a function
+    x="Cheery"
+func()
+
+print(x)
+
+"""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+""" 
+Example	Data Type	
+x = "Hello World"	str	
+x = 20	int	
+x = 20.5	float	
+x = 1j	complex	
+x = ["apple", "banana", "cherry"]	list	
+x = ("apple", "banana", "cherry")	tuple	
+x = range(6)	range	
+x = {"name" : "John", "age" : 36}	dict	
+x = {"apple", "banana", "cherry"}	set	
+x = frozenset({"apple", "banana", "cherry"})	frozenset	
+x = True	bool	
+x = b"Hello"	bytes	
+x = bytearray(5)	bytearray	
+x = memoryview(bytes(5))	memoryview	
+x = None	NoneType
+"""
