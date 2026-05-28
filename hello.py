02c0ffc1c62703cee250be84c559c06afa18b288
Soham Biswas <sohambis.in@gmail.com>
2026-05-28 23:01:17 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -123,5 +123,20 @@ if "expensive" not in txt:
 b="Hello World"
 print(b[2:7])
 print(b[:5])
-print(b[2:]).
+print(b[2:])
 print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
