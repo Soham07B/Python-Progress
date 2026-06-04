db13a55cfb178cc5e9de5acfd0298a40387e938e
Soham Biswas <sohambis.in@gmail.com>
2026-06-04 23:37:52 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -38,7 +38,7 @@ A variable name cannot start with a number
 A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 Variable names are case-sensitive (age, Age and AGE are three different variables)
 A variable name cannot be any of the Python keywords.
-"""
+""".
 
 
 """
