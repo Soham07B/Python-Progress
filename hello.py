bec8900238c92366497a7bc8aaf76b1a6301d7ed
Soham Biswas <sohambis.in@gmail.com>
2026-07-04 23:15:06 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -145,4 +145,10 @@ print(a.split(",")) # returns ['Hello', ' World!']
 
 thisList=["apple","cherry","mango","apple"]
 print (thisList)
-print ((len)thisList)..
+print ((len)thisList)
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
